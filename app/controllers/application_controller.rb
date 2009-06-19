7e04f5261aacdbe441b458d2ec852dec1ba685da
Bruce Robertson <brucerob@Ham.(none)>
2009-06-19 15:07:55 -0300

    remove broken xhtml_content_type plugin; force mimetype

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   include AuthenticatedSystem
-  sends_xhtml_with_correct_content_type
   helper_method :authorized?
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,4 +3,5 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
- Mime::Type.register "text/html", :html
+# Mime::Type.register "text/html", :html
+Mime::Type.register "application/xhtml+xml", :html
