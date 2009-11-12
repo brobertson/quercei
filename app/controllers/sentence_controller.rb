fd406af040931876f6c68f308ad88b2d52a9943a
Bruce G. Robertson <brucerob@heml.(none)>
2009-11-11 21:49:34 -0400

    Make it possible for a subdoc to begin on a sentence greater than 0 by searching for the first sentence in the subdoc if the controller is passed a sentence number of 0. Also, test if the previous sentence exists not by assuming 0 is the smallest possible number, but rather by testing if the decremented sentence number exists.

--- app/controllers/sentence_controller.rb
+++ app/controllers/sentence_controller.rb
@@ -4,15 +4,18 @@ class SentenceController < ApplicationController
 def index
  @tagset = ["PRED", "SBJ", "OBJ", "ATR", "ADV", "ATV/AtvV", "PNOM", "OCOMP", "COORD", "APOS", "AuxP", "AuxC", "AuxR", "AuxY", "AuxX", "AuxG", "AuxK", "AuxV", "AuxZ", "ExD", "PRED_CO", "SBJ_CO", "OBJ_CO", "ATR_CO", "ADV_CO", "ATV/AtvV_CO", "PNOM_CO", "OCOMP_CO", "ADV_AP", "ATR_AP", "COORD_AP", "OBJ_AP"] 
  @graphTypes = ["dot", "neato"]
- 
-  @sentence_number = params[:sentence]
+  @document_urn = params[:document_urn]
+  @subdoc = params[:subdoc]
+  if  params[:sentence] == "0" 
+   @sentence_number = Word.first(:all, :order => "sentence", :conditions => ["document_urn = ? and subdoc = ?", @document_urn, @subdoc]).sentence   
+  
+  else  
+    @sentence_number = params[:sentence]
+  end
   @next_sentence_number = 1 + @sentence_number.to_i
   @previous_sentence_number = @sentence_number.to_i - 1 
-  @document_urn = params[:document_urn] 
-  @subdoc = params[:subdoc]
   @words = Word.find(:all, :order => "number", :conditions => ["document_urn = ? and sentence = ? and subdoc = ?", @document_urn, @sentence_number, @subdoc])
   @next_sentence_exists = Word.exists?(["document_urn = ? and sentence = ? and subdoc = ?", @document_urn, @next_sentence_number, @subdoc])
-  @previous_sentence_exists = (@sentence_number.to_i > 0)
-  #@subdoc = @words.first(:subdoc)
+  @previous_sentence_exists = Word.exists?(["document_urn = ? and sentence = ? and subdoc = ?", @document_urn, @previous_sentence_number, @subdoc])
 end
 end
